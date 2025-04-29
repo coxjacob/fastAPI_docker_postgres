6cd73a6835a3c6ee77526481b56007144b46eec2
“coxjacob” <jacobcox1974@gmail.com>
2025-04-28 22:58:12 -0400

    Completed test cases

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ from fastapi import FastAPI
 from app.db import get_db_connection
 from app.student import Student, StudentCreate
 
+from typing import List
+
 app = FastAPI(title="University API")
 
 
@@ -40,3 +42,39 @@ def create_student(student: StudentCreate):
         return Student(**new_student)
     
     raise HTTPException(status_code=400, detail="Error creating student")
+
+
+@app.get("/students/", response_model=List[Student])
+def get_all_students():
+    students = []
+    conn, cursor = get_db_connection()
+    try:
+        cursor.execute("SELECT * FROM Students;")
+        rows = cursor.fetchall()
+        for row in rows:
+            students.append(Student(**row))
+        cursor.close()
+        print("Completed get_all_students")
+    except Exception as e:
+        conn.close()
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    conn.close()
+    if students:
+        return students
+    raise HTTPException(status_code=400, detail="Student not found")
+
+
+@app.delete("student/{student_id}")
+def delete_student(student_id: int):
+    conn, cursor = get_db_connection()
+    try:
+        cursor.execute(f"DELETE FROM Students WHERE id = {student_id}")
+        conn.commit()
+        cursor.close()
+    except Exception as e:
+        conn.close()
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    conn.close()
+    return {"message": "Student deleted successfully"}
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -21,10 +21,24 @@ def test_when_given_user_name_should_create_a_student_in_db(client):
     rand_value = randint(0, 100000)
     first_name = f"John {rand_value}"
     last_name = f"World {rand_value}"
-    create_response = client.post("/student/", json={
-                                  "first_name": first_name,
-                                  "last_name":last_name
-                                  })
+    try: 
+        create_response = client.post("/student/", json={
+                                    "first_name": first_name,
+                                    "last_name":last_name
+                                    })
+    except Exception as e:
+        print (str(e))
     assert create_response.status_code == 200
     student_id = create_response.json()['id']
     assert student_id != None
+
+def test_get_all_students_should_return_list_of_students(client):
+    response = client.get("/students/")
+    assert response.status_code==200
+    data = response.json()
+    assert isinstance(data, list)
+    assert len(data) > 0
+    for student in data: 
+        assert "id" in student
+        assert "first_name" in student
+        assert "last_name" in student
